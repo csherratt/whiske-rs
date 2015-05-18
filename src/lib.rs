ba6431032ebf3acb1988081a5e2c6344914dfd8d
Colin Sherratt <colin.sherratt@gmail.com>
2015-05-18 15:13:43 -0400

    Added more

--- src/lib.rs
+++ src/lib.rs
@@ -6,6 +6,7 @@ extern crate pulse;
 #[macro_use]
 extern crate gfx;
 
+use std::iter::FromIterator;
 use entity::*;
 use snowstorm::channel::*;
 
@@ -24,19 +25,85 @@ pub struct Material(Entity);
 
 /// A handle for a vertex buffer
 #[derive(Copy, Clone, Hash, Debug)]
-pub struct VertexBuffer(Entity);
+pub struct VertexBuffer(Entity, Length);
+
+#[derive(Copy, Clone, Hash, Debug)]
+pub enum Length {
+    Unsized,
+    Length(u32)
+}
 
 impl VertexBuffer {
     /// Create a vertex buffer
     pub fn new() -> VertexBuffer {
-        VertexBuffer(Entity::new())
+        VertexBuffer(Entity::new(), Length::Unsized)
     }
 
     /// Binds an a component to the Entity
-    pub fn bind<T>(self, data: T) -> EntityBinding<VertexBuffer, (T,)> {
-        EntityBinding::new(self, data)
+    pub fn bind(mut self, data: Vertex) -> VertexBufferBinding<(Vertex,)> {
+        self.1 = data.length();
+        VertexBufferBinding::new(self, data)
+    }
+
+    /// Get the length of a vertex buffer, Returns
+    /// None if the vertex buffer has no associated length
+    pub fn length(&self) -> Option<u32> {
+        match self.1 {
+            Length::Unsized => None,
+            Length::Length(x) => Some(x)
+        }
     }
 }
+
+/// A Entity & Some data that is associated with it
+#[derive(Copy, Clone)]
+pub struct VertexBufferBinding<T> {
+    entity: VertexBuffer,
+    data: T
+}
+
+impl<T> VertexBufferBinding<(T,)> {
+    pub fn new(entity: VertexBuffer, data: T) -> VertexBufferBinding<(T,)> {
+        VertexBufferBinding {
+            entity: entity,
+            data: (data,)
+        }
+    }
+}
+
+impl<T> VertexBufferBinding<T> {
+    /// Bind an additional component to the VertexBufferBinding
+    #[inline]
+    pub fn bind_index<O>(mut self, data: Vec<u32>) -> VertexBufferBinding<O>
+        where T: Append<Vec<u32>, Output=O> {
+        self.entity.1 = data.length();
+        VertexBufferBinding {
+            entity: self.entity,
+            data: self.data.append(data)
+        }
+    }
+}
+
+impl<A> VertexBufferBinding<(A,)> {
+    pub fn write<W>(self, sink: &mut W) -> VertexBuffer
+        where W: WriteEntity<VertexBuffer, A>{
+
+        sink.write(self.entity, self.data.0);
+        self.entity
+    }
+}
+
+impl<A, B> VertexBufferBinding<(A, B)> {
+    pub fn write<W>(self, sink: &mut W) -> VertexBuffer
+        where W: WriteEntity<VertexBuffer, A>+WriteEntity<VertexBuffer, B> {
+
+        let (a, b) = self.data;
+        sink.write(self.entity, a);
+        sink.write(self.entity, b);
+        self.entity
+    }
+}
+
 
 #[derive(Clone, Copy, Debug, Hash)]
 pub enum Primative {
@@ -82,7 +149,7 @@ gfx_vertex!( VertexPosTexNorm {
     a_Texture@ texture: [f32; 2],
 });
 
-#[derive(Clone)]
+#[derive(Clone, Debug)]
 pub enum Vertex {
     Pos(Vec<VertexPos>),
     PosTex(Vec<VertexPosTex>),
@@ -91,14 +158,84 @@ pub enum Vertex {
 }
 pub use Vertex::*;
 
+impl FromIterator<VertexPos> for Vertex {
+    fn from_iter<T>(iter: T) -> Vertex where T: IntoIterator<Item=VertexPos> {
+        Pos(iter.into_iter().collect())
+    }
+}
+
+impl FromIterator<VertexPosTex> for Vertex {
+    fn from_iter<T>(iter: T) -> Vertex where T: IntoIterator<Item=VertexPosTex> {
+        PosTex(iter.into_iter().collect())
+    }
+}
+
+impl FromIterator<VertexPosNorm> for Vertex {
+    fn from_iter<T>(iter: T) -> Vertex where T: IntoIterator<Item=VertexPosNorm> {
+        PosNorm(iter.into_iter().collect())
+    }
+}
+
+impl FromIterator<VertexPosTexNorm> for Vertex {
+    fn from_iter<T>(iter: T) -> Vertex where T: IntoIterator<Item=VertexPosTexNorm> {
+        PosTexNorm(iter.into_iter().collect())
+    }
+}
+
+// Wrapper to get the length of a buffer
+pub trait GetLength {
+    fn length(&self) -> Length;
+}
+
+impl<T> GetLength for Vec<T> {
+    fn length(&self) -> Length {
+        Length::Length(self.len() as u32)
+    }
+}
+
+impl GetLength for Vertex {
+    fn length(&self) -> Length {
+        Length::Length(match self {
+            &Vertex::Pos(ref x)         => x.len(),
+            &Vertex::PosTex(ref x)      => x.len(),
+            &Vertex::PosNorm(ref x)     => x.len(),
+            &Vertex::PosTexNorm(ref x)  => x.len()
+        } as u32)
+    }
+}
+
 impl VertexBuffer {
     /// Use the entire vertex buffer with the primative as a geometry
     pub fn geometry(&self, primative: Primative) -> GeometryData {
-        self.subbuffer(0, 0xFFFF_FFFF).geometry(primative)
+        self.subbuffer(0, 0).geometry(primative)
+    }
+
+    /// Convert the VertexBuffer into a subbuffer the includes the entire
+    /// range of the VertexBuffer
+    pub fn entire(&self) -> VertexSubBuffer {
+        let max = self.length().expect("VertexBuffer was not bound to any buffer. Cannot use as subbuffer.");
+        self.subbuffer(0, max)
+    }
+
+    /// Convert the VertexBuffer into a subbuffer the includes the entire
+    /// Buffers starting at start
+    pub fn from(&self, start: u32) -> VertexSubBuffer {
+        let max = self.length().expect("VertexBuffer was not bound to any buffer. Cannot use as subbuffer.");
+        self.subbuffer(start, max-start)
+    }
+
+    /// Convert the VertexBuffer into a subbuffer the includes the entire
+    /// Buffers starting at start
+    pub fn up_to(&self, end: u32) -> VertexSubBuffer {
+        self.subbuffer(0, end)
     }
 
     /// Use a section of the buffer as a subbuffer
     pub fn subbuffer(&self, start: u32, length: u32) -> VertexSubBuffer {
+        let max = self.length().expect("VertexBuffer was not bound to any buffer. Cannot use as subbuffer.");
+        assert!(start < max);
+        assert!(max >= start + length);
+
         VertexSubBuffer {
             parent: self.0,
             start: start,
@@ -116,39 +253,69 @@ impl VertexSubBuffer {
         }
     }
 
+    /// Convert the VertexBuffer into a subbuffer the includes the entire
+    /// range of the VertexBuffer
+    pub fn entire(&self) -> VertexSubBuffer {
+        self.subbuffer(0, self.length)
+    }
+
+    /// Convert the VertexBuffer into a subbuffer the includes the entire
+    /// Buffers starting at start
+    pub fn from(&self, start: u32) -> VertexSubBuffer {
+        let length = self.length - start;
+        self.subbuffer(start, length)
+    }
+
+    /// Convert the VertexBuffer into a subbuffer the includes the entire
+    /// Buffers starting at start
+    pub fn up_to(&self, end: u32) -> VertexSubBuffer {
+        self.subbuffer(0, end)
+    }
+
     /// Use a section of the buffer as a subbuffer
     pub fn subbuffer(&self, start: u32, length: u32) -> VertexSubBuffer {
+        assert!(start < self.length);
+        assert!(start + length < self.length);
+
         VertexSubBuffer {
             parent: self.parent,
-            start: start + self.start,
+            start: start,
             length: length
         }
     }
 }
 
+#[derive(Clone, Debug)]
 pub enum VertexData {
     Vertex(Vertex),
     Index(Vec<u32>)
 }
 
+#[derive(Clone, Debug)]
 pub enum Message {
-    Vertex(Operation<VertexBuffer, VertexData>)
+    Vertex(Operation<Entity, VertexData>)
 }
 
-pub struct GraphicsSource(Sender<Message>);
-pub struct GraphicsSink(Receiver<Message>);
+#[derive(Clone)]
+pub struct GraphicsSource(pub Sender<Message>);
+pub struct GraphicsSink(pub Receiver<Message>);
+
 
 impl GraphicsSource {
     pub fn new() -> (GraphicsSink, GraphicsSource) {
         let (vx_tx, vx_rx) = channel();
         (GraphicsSink(vx_rx), GraphicsSource(vx_tx))
     }
+
+    pub fn next_frame(&mut self) {
+        self.0.next_frame();
+    }
 }
 
 impl WriteEntity<VertexBuffer, Vertex> for GraphicsSource {
     fn write(&mut self, entity: VertexBuffer, data: Vertex) {
         self.0.send(Message::Vertex(
-            Operation::Upsert(entity, VertexData::Vertex(data))
+            Operation::Upsert(entity.0, VertexData::Vertex(data))
         ))
     }
 }
@@ -156,7 +323,7 @@ impl WriteEntity<VertexBuffer, Vertex> for GraphicsSource {
 impl WriteEntity<VertexBuffer, Vec<u32>> for GraphicsSource {
     fn write(&mut self, entity: VertexBuffer, data: Vec<u32>) {
         self.0.send(Message::Vertex(
-            Operation::Upsert(entity, VertexData::Index(data))
+            Operation::Upsert(entity.0, VertexData::Index(data))
         ))
     }
 }
--- tests/tests.rs
+++ tests/tests.rs
@@ -5,9 +5,19 @@ use graphics::*;
 
 #[test]
 fn create_vb() {
-    let (_, mut source) = GraphicsSource::new();
-    let vb = VertexBuffer::new()
-                          .bind(Pos(Vec::new()))
-                          .bind(Vec::new())
-                          .write(&mut source);
+    let (sink, mut source) = GraphicsSource::new();
+
+    let a = VertexBuffer::new()
+        .bind((0..100).map(|_| VertexPos{position: [0., 0., 0.]}).collect())
+        .write(&mut source);
+    assert_eq!(100, a.length().unwrap());
+
+    let b = VertexBuffer::new()
+        .bind((0..100).map(|_| VertexPos{position: [0., 0., 0.]}).collect())
+        .bind_index((0..1000).collect())
+        .write(&mut source);
+    assert_eq!(1000, b.length().unwrap());
+
+    drop(source);
+    drop(sink);
 }
