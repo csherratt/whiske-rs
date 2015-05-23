c56869f14a43be072f4788f070f61ed7a7d4c254
Colin Sherratt <colin.sherratt@gmail.com>
2015-05-23 15:45:23 -0400

    Inital

---
+++ examples/cube.rs
@@ -0,0 +1,127 @@
+extern crate engine;
+extern crate fibe;
+extern crate snowstorm;
+extern crate render;
+extern crate position;
+extern crate graphics;
+extern crate parent;
+extern crate genmesh;
+extern crate cgmath;
+extern crate entity;
+
+use std::thread;
+use fibe::task;
+use snowstorm::channel::*;
+
+use graphics::{Vertex, VertexPosTexNorm, PosTexNorm, VertexBuffer,
+    Geometry, Material, Primative, KdFlat, DrawBinding
+};
+use genmesh::generators::{Plane, Cube, SphereUV};
+use genmesh::{MapToVertices, Indexer, LruIndexer};
+use genmesh::{Vertices, Triangulate, Quad, Polygon};
+use cgmath::{Vector3, EuclideanVector, Decomposed, Transform};
+
+use entity::Entity;
+use position::Delta;
+
+fn build_vectors<T: Iterator<Item=Quad<VertexPosTexNorm>>>(input: T)
+    -> (Vertex, Vec<u32>) {
+
+    let mut mesh_data: Vec<VertexPosTexNorm> = Vec::new();
+    let index: Vec<u32> = {
+        let mut indexer = LruIndexer::new(16, |_, v| mesh_data.push(v));
+        input.map(|mut p: Quad<VertexPosTexNorm>| {
+            let a = Vector3::new(p.x.position[0],
+                                 p.x.position[1],
+                                 p.x.position[2]);
+            let b = Vector3::new(p.y.position[0],
+                                 p.y.position[1],
+                                 p.y.position[2]);
+            let c = Vector3::new(p.z.position[0],
+                                 p.z.position[1],
+                                 p.z.position[2]);
+
+            let normal = (a - b).cross(&(b - c)).normalize();
+
+            p.x.normal = [normal.x, normal.y, normal.z];
+            p.y.normal = [normal.x, normal.y, normal.z];
+            p.z.normal = [normal.x, normal.y, normal.z];
+            p.w.normal = [normal.x, normal.y, normal.z];
+
+            p.x.texture = [-1., -1.];
+            p.y.texture = [-1.,  1.];
+            p.z.texture = [-1., -1.];
+            p.w.texture = [-1.,  1.];
+
+            p
+        })
+        .vertex(|v| indexer.index(v) as u32)
+        .triangulate()
+        .vertices()
+        .collect()
+    };
+
+    (PosTexNorm(mesh_data), index)
+}
+
+fn main() {
+    let mut engine = engine::Engine::new();
+
+    for i in 0..1 {
+        engine.start_input_processor(|sched, msgs| {
+
+        });
+    }
+
+    let (mut tx_parent, rx) = channel();
+    let rx_parent = parent::parent(engine.sched(), rx);
+
+    let (mut tx_position, rx) = channel();
+    let rx_position = position::position(engine.sched(), rx, rx_parent.clone());
+
+    let (mut gsink, mut gsrc) = graphics::GraphicsSource::new();
+
+    let (cube, mat) = {
+        let (cube_v, cube_i) = build_vectors(
+            Cube::new().vertex(|(x, y, z)| {
+                VertexPosTexNorm { position: [x, y, z], normal: [0., 0., 0.], texture: [0., 0.] }
+            })
+        );
+
+        let vb = VertexBuffer::new().bind(cube_v).bind_index(cube_i).write(&mut gsrc);
+        let geo = Geometry::new().bind(vb.geometry(Primative::Triangle)).write(&mut gsrc);
+        let mat = Material::new().bind(KdFlat([1., 0., 0.])).write(&mut gsrc);
+        (geo, mat)
+    };
+
+    let count = 5;
+
+    for x in (-count..count) {
+        for y in (-count..count) {
+            for z in (-count..count) {
+                let eid = Entity::new().bind(DrawBinding(cube, mat)).write(&mut gsrc);
+                let mut pos = Delta(Decomposed::identity());
+                pos.0.disp.x = x as f32 * 5.;
+                pos.0.disp.y = y as f32 * 5.;
+                pos.0.disp.z = z as f32 * 5.;
+                eid.bind(pos).write(&mut tx_position);
+            }
+        }
+    }
+
+    let eid = Entity::new().bind(DrawBinding(cube, mat)).write(&mut gsrc);
+    eid.bind(Delta(Decomposed::identity())).write(&mut tx_position);
+
+    engine.start_render(|_, render, device|{
+        let mut render = render::Render::new(gsink, rx_position, render, device);
+        Box::new(move |sched, stream| {
+        	println!("Render!");
+        	tx_parent.next_frame();
+        	tx_position.next_frame();
+        	gsrc.next_frame();
+        	render.draw(sched, stream);
+        })
+    });
+
+    engine.run();
+}
---
+++ src/lib.rs
@@ -0,0 +1,313 @@
+extern crate entity;
+extern crate position;
+extern crate graphics;
+extern crate snowstorm;
+extern crate fibe;
+#[macro_use]
+extern crate gfx;
+extern crate gfx_phase;
+extern crate gfx_scene;
+extern crate gfx_device_gl;
+extern crate gfx_pipeline;
+extern crate engine;
+extern crate draw_queue;
+extern crate pulse;
+extern crate cgmath;
+
+use std::collections::HashMap;
+use snowstorm::channel;
+use position::Solved;
+use graphics::{
+    GraphicsSink, Message, VertexData,
+    Vertex, Pos, PosTex, PosNorm, PosTexNorm,
+    MaterialComponent, GeometryData, DrawBinding
+};
+use engine::Window;
+use pulse::{Signal, SelectMap, Signals};
+use entity::{Entity, Operation};
+
+use gfx::{
+    Mesh, handle, BufferRole, Factory,
+    Slice, PrimitiveType, SliceKind
+};
+use gfx::batch::OwnedBatch;
+use gfx::traits::FactoryExt;
+use gfx_device_gl::{Device, Resources};
+use gfx_phase::{Technique, Phase};
+use gfx_scene::{AbstractScene, Camera, Report, Error, Context, Frustum};
+use gfx_pipeline::{Material, Transparency, forward, Pipeline};
+
+use cgmath::{AffineMatrix3, Matrix4, Bound, Relation, BaseFloat, Decomposed, Vector3, Quaternion, Transform};
+
+struct GeometrySlice {
+    mesh: Mesh<Resources>,
+    slice: Slice<Resources>
+}
+
+#[derive(Copy, Clone, Debug)]
+struct NullBound;
+
+impl<S: BaseFloat> Bound<S> for NullBound {
+    fn relate_plane(&self, p: &cgmath::Plane<S>) -> Relation {
+        Relation::In
+    }
+}
+
+pub struct Render {
+    device: Device,
+    factory: gfx_device_gl::Factory,
+
+    graphics: graphics::GraphicsSink,
+    pos_input: channel::Receiver<Operation<Entity, Solved>>,
+
+    position: Position,
+
+    vertex: HashMap<Entity, (Option<Mesh<Resources>>, Option<handle::Buffer<Resources, u32>>)>,
+    materials: HashMap<Entity, Material<Resources>>,
+    geometry_data: HashMap<Entity, GeometryData>,
+    geometry_slice: HashMap<Entity, GeometrySlice>,
+
+    binding: HashMap<Entity, DrawBinding>,
+    to_draw: HashMap<Entity, gfx_scene::Entity<Resources, Material<Resources>, Position, NullBound>>,
+
+    pipeline: Option<forward::Pipeline<Resources>>
+}
+
+pub struct Position(pub HashMap<Entity, Solved>);
+
+impl gfx_scene::World for Position {
+    type Scalar = f32;
+    type Transform = Decomposed<f32, Vector3<f32>, Quaternion<f32>>;
+    type NodePtr = Entity;
+    type SkeletonPtr = ();
+
+    fn get_transform(&self, node: &Entity) -> Decomposed<f32, Vector3<f32>, Quaternion<f32>> {
+        self.0.get(node).unwrap().0
+    }
+}
+
+impl AbstractScene<Resources> for Render {
+    type ViewInfo = gfx_pipeline::ViewInfo<f32>;
+    type Material = Material<Resources>;
+    type Camera = Camera<cgmath::Ortho<f32>, Entity>;
+
+    fn draw<H, S>(&self,
+                  phase: &mut H,
+                  camera: &Camera<cgmath::Ortho<f32>, Entity>,
+                  stream: &mut S) -> Result<Report, Error> where
+        H: gfx_phase::AbstractPhase<Resources, Material<Resources>, gfx_pipeline::ViewInfo<f32>>,
+        S: gfx::Stream<Resources>,
+    
+    {   
+        let mut culler = Frustum::new();
+        let x = Context::new(&self.position, &mut culler, camera)
+                        .draw(self.to_draw.values(), phase, stream);
+        println!("{:?}", x);
+        x
+    }
+}
+
+impl Render {
+    pub fn new(graphics: GraphicsSink,
+               position: channel::Receiver<Operation<Entity, Solved>>,
+               device: Device,
+               mut factory: gfx_device_gl::Factory) -> Render {
+
+        let pipeline = forward::Pipeline::new(&mut factory);
+
+        Render {
+            device: device,
+            factory: factory,
+            graphics: graphics,
+            pos_input: position,
+            position: Position(HashMap::new()),
+            vertex: HashMap::new(),
+            materials: HashMap::new(),
+            geometry_data: HashMap::new(),
+            geometry_slice: HashMap::new(),
+            to_draw: HashMap::new(),
+            binding: HashMap::new(),
+            pipeline: Some(pipeline.unwrap())
+        }
+    }
+
+    fn add_vertex(&mut self, entity: Entity, vertex: VertexData) {
+        let dst = self.vertex.entry(entity).or_insert_with(|| (None, None));
+        match vertex {
+            VertexData::Vertex(Pos(data)) => {
+                dst.0 = Some(self.factory.create_mesh(&data[..]));
+            }
+            VertexData::Vertex(PosTex(data)) => {
+                dst.0 = Some(self.factory.create_mesh(&data[..]));
+            }
+            VertexData::Vertex(PosNorm(data)) => {
+                dst.0 = Some(self.factory.create_mesh(&data[..]));
+            }
+            VertexData::Vertex(PosTexNorm(data)) => {
+                dst.0 = Some(self.factory.create_mesh(&data[..]));
+            }
+            VertexData::Index(data) => {
+                dst.1 = Some(self.factory.create_buffer_static(&data, BufferRole::Index));
+            }
+        }
+    }
+
+    fn add_material(&mut self, entity: Entity, material: MaterialComponent) {
+        let dst = self.materials.entry(entity)
+                      .or_insert_with(|| {
+                       Material {
+                            color: [0., 0., 0., 1.],
+                            texture: None,
+                            transparency: Transparency::Opaque
+                       }});
+
+        match material {
+            MaterialComponent::KdFlat(x) => {
+                dst.color[0] = x[0];
+                dst.color[1] = x[1];
+                dst.color[2] = x[2];
+            }
+            _ => ()
+        }
+    }
+
+    fn add_geometry(&mut self, entity: Entity, geometry: GeometryData) {
+        self.geometry_data.insert(entity, geometry);
+
+        match self.vertex.get(&geometry.buffer.parent) {
+            Some(&(Some(ref v), None)) => {
+                Some(GeometrySlice {
+                    mesh: v.clone(),
+                    slice: Slice {
+                        start: geometry.buffer.start,
+                        end: geometry.buffer.start + geometry.buffer.length,
+                        prim_type: PrimitiveType::TriangleList,
+                        kind: SliceKind::Vertex
+                    }
+                })
+            }
+            Some(&(Some(ref v), Some(ref i))) => {
+                Some(GeometrySlice {
+                    mesh: v.clone(),
+                    slice: Slice {
+                        start: geometry.buffer.start,
+                        end: geometry.buffer.start + geometry.buffer.length,
+                        prim_type: PrimitiveType::TriangleList,
+                        kind: SliceKind::Index32(i.clone(), 0)
+                    }
+                })
+            }
+            _ => None
+        }.map(|slice| {
+            self.geometry_slice.insert(entity, slice);
+        });
+    }
+
+    fn add_binding(&mut self, entity: Entity, draw: DrawBinding) {
+        self.binding.insert(entity, draw);
+
+        match (self.geometry_slice.get(&(draw.0).0), self.materials.get(&(draw.1).0)) {
+            (Some(a), Some(b)) => {
+                Some(gfx_scene::Entity{
+                    name: "".to_string(),
+                    visible: true,
+                    mesh: a.mesh.clone(),
+                    node: entity,
+                    skeleton: None,
+                    bound: NullBound,
+                    fragments: vec![gfx_scene::Fragment{
+                        material: b.clone(),
+                        slice: a.slice.clone()
+                    }]
+                })
+            }
+            _ => None
+        }.map(|e| self.to_draw.insert(entity, e));
+    }
+
+    fn sync_graphics(&mut self) -> Option<Signal> {
+        while let Some(msg) = self.graphics.0.try_recv() {
+            match msg {
+                Message::Vertex(Operation::Upsert(eid, vd)) => {
+                    self.add_vertex(eid, vd);
+                }
+                Message::Vertex(Operation::Delete(eid)) => {
+                    self.vertex.remove(&eid);
+                }
+                Message::Material(Operation::Upsert(eid, mat)) => {
+                    self.add_material(eid, mat);
+                }
+                Message::Material(Operation::Delete(eid)) => {
+                    self.materials.remove(&eid);
+                }
+                Message::Geometry(Operation::Upsert(eid, geo)) => {
+                    self.add_geometry(eid, geo);
+                }
+                Message::Geometry(Operation::Delete(eid)) => {
+                    self.geometry_data.remove(&eid);
+                    self.geometry_slice.remove(&eid);
+                }
+                Message::DrawBinding(Operation::Upsert(eid, geo)) => {
+                    self.add_binding(eid, geo);
+                }
+                Message::DrawBinding(Operation::Delete(eid)) => {
+
+                }
+            }
+        }
+
+        if self.graphics.0.closed() {
+            None
+        } else {
+            Some(self.graphics.0.signal())
+        }
+    }
+
+    fn sync_position(&mut self) -> Option<Signal> {
+        for msg in self.pos_input.copy_iter(false) {
+            msg.write(&mut self.position.0);
+        }
+
+        if self.pos_input.closed() {
+            None
+        } else {
+            Some(self.pos_input.signal())
+        }
+    }
+
+    fn sync(&mut self) {
+        let mut select: SelectMap<fn(&mut Render) -> Option<Signal>> = SelectMap::new();
+        select.add(self.graphics.0.signal(), Render::sync_graphics);
+        select.add(self.pos_input.signal(), Render::sync_position);
+
+        while let Some((_, cb)) = select.next() {
+            if let Some(s) = cb(self) {
+                select.add(s, cb);
+            }
+        }
+
+        self.graphics.0.next_frame();
+        self.pos_input.next_frame();
+    }
+
+    /// 
+    pub fn draw(&mut self, _: &mut fibe::Schedule, window: &mut Window) {
+        self.sync();
+        let eid = Entity::new();
+        let camera = gfx_scene::Camera {
+            name: "Cam".to_string(),
+            projection: cgmath::Ortho {
+                left: -50., right: 50.,
+                bottom: -50., top: 50.,
+                near: -50f32, far: 50f32,
+            },
+            node: eid
+        };
+        self.position.0.insert(eid, Solved(Decomposed::identity()));
+        let mut pipeline = self.pipeline.take().unwrap();
+        pipeline.render(self, &camera, window);
+        self.pipeline = Some(pipeline);
+        window.present(&mut self.device);
+    }
+}
+
