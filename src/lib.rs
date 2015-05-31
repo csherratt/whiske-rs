2d7a85ea025ee31b72ac89c6337232f00d66a7b4
Colin Sherratt <colin.sherratt@gmail.com>
2015-05-30 20:09:56 -0400

    Make window generic

--- src/lib.rs
+++ src/lib.rs
@@ -6,27 +6,29 @@ extern crate gfx_window_glutin;
 extern crate glutin;
 
 use fibe::*;
-use gfx_device_gl::{Device, Factory};
 use glutin::Event;
 
 pub use snowstorm::channel::*;
 
-pub type Window = gfx::extra::stream::OwnedStream<
-    gfx_device_gl::Device,
-    gfx_window_glutin::Output<gfx_device_gl::Resources>
+pub type Window<D, R> = gfx::extra::stream::OwnedStream<D,
+    gfx_window_glutin::Output<R>
 >;
 
-pub struct Engine {
+pub struct Engine<D: gfx::Device, F, R: gfx::Resources> {
     input: (Sender<Event>, Receiver<Event>),
     pool: fibe::Frontend,
-    window: Window,
-    render_args: Option<(Device, Factory)>,
-    render: Option<Box<FnMut(&mut fibe::Schedule, &mut Window)>>
+    window: Window<D, R>,
+    render_args: Option<(D, F)>,
+    render: Option<Box<FnMut(&mut fibe::Schedule, &mut Window<D, R>)>>
 }
 
-impl Engine {
+impl Engine<gfx_device_gl::Device,
+            gfx_device_gl::Factory,
+            gfx_device_gl::Resources> {
     /// Create a new Engine context
-    pub fn new() -> Engine {
+    pub fn new() -> Engine<gfx_device_gl::Device,
+                           gfx_device_gl::Factory,
+                           gfx_device_gl::Resources> {
         let (stream, device, factory) = gfx_window_glutin::init(
             glutin::Window::new().unwrap()
         );
@@ -39,10 +41,19 @@ impl Engine {
             render: None
         }
     }
+}
 
+
+impl<D, F, R> Engine<D, F, R>
+    where D: gfx::Device,
+          R: gfx::Resources
+
+ {
     /// Fetch a copy of the input stream and run actor
     /// with the input stream as a input
-    pub fn start_input_processor<F>(&mut self, actor: F) where F: FnOnce(&mut fibe::Schedule, Receiver<Event>)+Send+'static {
+    pub fn start_input_processor<C>(&mut self, actor: C)
+        where C: FnOnce(&mut fibe::Schedule, Receiver<Event>)+Send+'static {
+        
         let rx = self.input.1.clone();
         task(|sched| {
             actor(sched, rx);
@@ -51,8 +62,8 @@ impl Engine {
 
     /// Fetch a copy of the input stream and run actor
     /// with the input stream as a input
-    pub fn start_render<F>(&mut self, render: F)
-        where F: FnOnce(&mut fibe::Schedule, Device, Factory) -> Box<FnMut(&mut fibe::Schedule, &mut Window)> {
+    pub fn start_render<C>(&mut self, render: C)
+        where C: FnOnce(&mut fibe::Schedule, D, F) -> Box<FnMut(&mut fibe::Schedule, &mut Window<D, R>)> {
 
         let (device, factory) = self.render_args.take().expect("Only one render can be created");
         let render = render(&mut self.pool, device, factory);
