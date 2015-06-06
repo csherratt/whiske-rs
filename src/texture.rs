1c5dd96b2ddbfc31b0abd7eac7ae9c40c4d89aee
Colin Sherratt <colin.sherratt@gmail.com>
2015-06-06 18:16:52 -0400

    Use implicit future from task()

--- src/texture.rs
+++ src/texture.rs
@@ -3,8 +3,8 @@ use std::path::PathBuf;
 use entity::{Entity, EntityBinding};
 use fibe::{Schedule, task};
 use image;
-use future_pulse::Future;
 use GraphicsSource;
+use future_pulse::Future;
 
 /// A handle for a texture
 #[derive(Copy, Clone, Hash, Debug)]
@@ -19,19 +19,13 @@ impl Texture {
     /// Load a image from Path
     pub fn load(sched: &mut Schedule, path: PathBuf, mut src: GraphicsSource)
         -> Future<Result<Texture, image::ImageError>> {
-
-        let (res, set) = Future::new();
-        
+       
         task(move |_| {
-            set.set(
-                image::open(path)
-                      .map(|image| {
-                        Texture::new().bind(image).write(&mut src)
-                       })
-            )
-        }).start(sched);
-
-        res
+            image::open(path)
+                .map(|image| {
+                    Texture::new().bind(image).write(&mut src)
+                })
+        }).start(sched)
     }
 
     /// Bind some data to the Texture
